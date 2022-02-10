f6ac926c395cd0d3832b4a3347f88aa389f5cd1c
lwf504 <lwf504@york.ac.uk>
2022-02-10 14:11:40 +0000

    Commented code

--- src/main/java/maptrix/wiseguide/HelloController.java
+++ src/main/java/maptrix/wiseguide/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() throws IOException {
         welcomeText.setText("Welcome to JavaFX Application!");
-        client.ClientMain.main();
+        client.ClientMain.main(); // Pressing "Hello" button starts the client
     }
 }
