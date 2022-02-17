4d97b9ca0de32a677d53fe01e3506229d71e9964
jingham2510 <jsphingham@gmail.com>
2022-02-17 14:45:00 +0000

    First iteration of login page No functionality just looks nice :)

--- src/main/java/maptrix/wiseguide/HelloApplication.java
+++ src/main/java/maptrix/wiseguide/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 320);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
