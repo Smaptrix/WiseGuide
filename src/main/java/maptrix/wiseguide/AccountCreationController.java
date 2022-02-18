0337bb1d2d7bdc1df0210586c6ca917b2b9d85be
Jingham2510 <77728651+Jingham2510@users.noreply.github.com>
2022-02-18 20:01:18 +0000

    User Creation Window Added - Requires functionality

---
+++ src/main/java/maptrix/wiseguide/AccountCreationController.java
@@ -0,0 +1,30 @@
+package maptrix.wiseguide;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AccountCreationController {
+
+    @FXML
+    TextField userField;
+
+    @FXML
+    PasswordField passField;
+
+    @FXML
+    PasswordField passConfirmField;
+
+    @FXML
+    Label errLabel;
+
+    @FXML
+    Button createAccountButton;
+
+
+
+
+
+}
