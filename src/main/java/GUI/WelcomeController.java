138c876319e61479767d5c5571848b05bd82e2d5
WPITCHFORK <60607062+WPITCHFORK@users.noreply.github.com>
2022-02-20 00:39:35 +0100

    Removed fx:id tags until further research Updated file header comments

--- src/main/java/GUI/WelcomeController.java
+++ src/main/java/GUI/WelcomeController.java
@@ -3,31 +3,14 @@
     Project Name:   WiseGuide
     Authors:        Will Pitchfork
     Date Created:   04/02/2022
-    Last Updated:   04/02/2022
+    Last Updated:   20/02/2022
  */
 
 package GUI;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.AnchorPane;
 
 public class WelcomeController {
-    public AnchorPane mainPane;
-    public MenuBar menuBar;
-    public SplitPane horizontalSplitPane;
-    public AnchorPane leftPane;
-    public SplitPane leftSplitPane;
-    public TitledPane venuesPane;
-    public ScrollPane venueScrollPane;
-    public TitledPane routesPane;
-    public ScrollPane routesScrollPane;
-    public AnchorPane mapPane;
-
-
     @FXML
     protected void onHelloButtonClick() {
         
