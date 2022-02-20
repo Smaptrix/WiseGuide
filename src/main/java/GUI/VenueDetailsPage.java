d9515bbd80ea5f3c93600dddd24a493af2fbbcdb
WPITCHFORK <60607062+WPITCHFORK@users.noreply.github.com>
2022-02-20 01:47:06 +0100

    Created Venue details page skeleton Updated CSS Files slightly

---
+++ src/main/java/GUI/VenueDetailsPage.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class VenueDetailsPage extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(WelcomeApplication.class.getResource("VenueDetailsPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("WiseGuide by Maptrix - V1.0.0");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
