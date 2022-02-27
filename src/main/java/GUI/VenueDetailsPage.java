429ef9500332ca054b8fb1a88fa707930907f710
WPITCHFORK <60607062+WPITCHFORK@users.noreply.github.com>
2022-02-27 07:21:55 +0000

    Added logo to project file Updated login page to match other docs

--- src/main/java/GUI/VenueDetailsPage.java
+++ src/main/java/GUI/VenueDetailsPage.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -15,7 +14,8 @@ public class VenueDetailsPage extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(WelcomeApplication.class.getResource("VenueDetailsPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("WiseGuide by Maptrix - V1.0.0");
-
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
