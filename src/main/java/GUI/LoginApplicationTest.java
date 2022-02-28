761acc2e8b64a0585f5f206d52dec908b7210ae3
AidanCarson1 <aidancarson1@gmail.com>
2022-02-28 01:39:36 +0000

    Tests Update - Added unit tests and list of integration tests to implement.

--- src/main/java/GUI/LoginApplicationTest.java
+++ src/main/java/GUI/LoginApplicationTest.java
@@ -13,7 +13,12 @@ import org.junit.Test;
 import org.testfx.api.FxAssert;
 import org.testfx.api.FxToolkit;
 import org.testfx.assertions.api.Assertions;
+import org.testfx.assertions.api.TextAssert;
+import org.testfx.assertions.api.TextInputControlAssert;
 import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.matcher.base.WindowMatchers;
+import org.testfx.matcher.control.TextInputControlMatchers;
+import org.testfx.matcher.control.TextMatchers;
 
 import java.util.Objects;
 import java.util.concurrent.TimeoutException;
@@ -39,7 +44,7 @@ public class LoginApplicationTest extends ApplicationTest {
         FxToolkit.hideStage();
     }
 
-    @Test
+/*    @Test
     public void clickOnCreateAcc() throws TimeoutException {
         sleep(1000);
         moveTo("#createAccButton");
@@ -55,19 +60,145 @@ public class LoginApplicationTest extends ApplicationTest {
         clickOn("#userField");
         write("testUser");
 
-    }
+    }*/
 
 
+    /* === UNIT TESTS === */
+
+    //Unit tests that test all the basic stuff to make sure it all works.
+    //Not everything works right now. I don't know how to do half the assertions because I can't find any TestFX documentation.
+
+    //I think it would be useful if the app had methods that could be used by tests to open specific pages?
+    //Right now, to access the Account Created Confirmation "Okay" button, the test has to create a working account
+    //(integration test) to test that the OK button exists (unit test) which is not really ideal.
+
+
+    //Main Screen | Click Login | Unit Test
     @Test
-    public void clickOnLogin() {
+    public void clickOnLoginTest() {
         sleep(1000);
         clickOn("#loginButton");
+
+        //TODO: Clicking login when no data is entered produces - can't test until functionality is implemented.
     }
 
+    //Main Screen | Click Exit | Unit Test
     @Test
     public void clickOnExit() {
         sleep(1000);
         clickOn("#exitButton");
     }
+
+    //Main Screen | Click Create Account | Unit Test
+    @Test
+    public void clickOnCreateAccountTest(){
+        sleep(1000);
+        clickOn("#createAccButton");
+        FxAssert.verifyThat(window("Account Creation"), WindowMatchers.isShowing());
+    }
+
+    //Main Screen | Enter text in username field | Unit Test
+    @Test
+    public void enterMainScreenUsernameTest(){
+        sleep(1000);
+        clickOn("#usernameTextField");
+        sleep(1000);
+        write("Test");
+        sleep(1000);
+        FxAssert.verifyThat("#usernameTextField", TextInputControlMatchers.hasText("Test"));
+    }
+
+    //Enter text in password field | Unit Test
+    @Test
+    public void enterMainScreenPasswordTest(){
+        sleep(1000);
+        clickOn("#userPassField");
+        sleep(1000);
+        write("password");
+        sleep(1000);
+        FxAssert.verifyThat("#userPassField", TextInputControlMatchers.hasText("password"));
+    }
+
+    //Account Creation | Enter text in username field | Unit Test
+    @Test
+    public void enterCreateUsernameTest(){
+        sleep(1000);
+        clickOn("#createAccButton");
+        clickOn("#usernameField");
+        write("username");
+        FxAssert.verifyThat("#usernameField", TextInputControlMatchers.hasText("username"));
+    }
+
+    //Account Creation | Enter text in password field | Unit Test
+    @Test
+    public void enterCreatePasswordTest(){
+        sleep(1000);
+        clickOn("#createAccButton");
+        clickOn("#passField");
+        write("password");
+        FxAssert.verifyThat("#passField", TextInputControlMatchers.hasText("password"));
+    }
+
+    //Account Creation | Enter text in confirm password field | Unit Test
+    @Test
+    public void enterCreateConfirmPasswordTest(){
+        sleep(1000);
+        clickOn("#createAccButton");
+        clickOn("#passConfirmField");
+        write("password");
+        FxAssert.verifyThat("#passConfirmField", TextInputControlMatchers.hasText("password"));
+    }
+
+    //Account Creation | Click Age 13 Box | Unit Test
+    @Test
+    public void clickCreateCheckboxTest(){
+        sleep(1000);
+        clickOn("#createAccButton");
+        clickOn("#ageCheckBox");
+
+        //TODO: I don't know how to check the state of the checkbox yet.
+    }
+
+    //Account Creation | Click Create Account Button | Unit Test
+    @Test
+    public void createAccountButtonTest(){
+        sleep(1000);
+        clickOn("#createAccButton");
+        clickOn("#createAccountButton");
+
+        //TODO: How to check label text?
+    }
+
+    //Account Creation | Open Privacy Policy/T&C | Unit Test
+    //StackOverflow says this isn't possible to test.
+    //https://stackoverflow.com/questions/13085791/possible-to-check-if-a-website-is-open-in-browser-from-java?rq=1
+
+    //TODO: Account Creation Confirmation | Click "Okay" | Unit Test
+    //Need to create a working account before this can be tested. Not ideal - investigate alternative approach.
+
+    //TODO: Account Creation | ANY LIMITS ON WHAT CHARACTERS ALLOWED IN USER/PASS??
+
+
+    /* === INTEGRATION TESTS === */
+
+    //TODO: Main Screen | Login real user real password | Integration Test with Login System
+
+    //TODO: Main Screen | Login real user fake password | Integration Test with Login System
+
+    //TODO: Main Screen | Login fake user fake password | Integration Test with Login System
+
+    //TODO: Account Creation | Create New User | Integration Test with Login System
+
+    //TODO: Account Creation | Create Existing User Attempt | Integration Test with Login System
+
+    //TODO: Account Creation | Create User Attempt - Don't check 13 box
+    //TODO: Account Creation | Create Blank User Attempt
+    //TODO: Account Creation | Create User Attempt - Username Only
+    //TODO: Account Creation | Create User Attempt - Password Only
+    //TODO: Account Creation | Create User Attempt - Confirm Password Only
+    //TODO: Account Creation | Create User Attempt - Username & Pass Only
+    //TODO: Account Creation | Create User Attempt - Username & Confirm Only
+    //TODO: Account Creation | Create User Attempt - Pass & Confirm Only
+    //TODO: Account Creation | Create User Attempt - Mismatched Passwords
 
 }
