e79a8ae10cda72833e327316816005c80a8f49b4
Jingham2510 <77728651+Jingham2510@users.noreply.github.com>
2022-03-17 22:06:23 +0000

    Rebase branch ontop of main updates

--- src/main/java/GUI/LoginController.java
+++ src/main/java/GUI/LoginController.java
@@ -10,23 +10,19 @@ package GUI;
 
 
 import client.Client;
-import GUI.LoginApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import server.ServerUserHandler;
 import serverclientstuff.User;
 
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
+
+
 
 public class LoginController {
-
 
 
     //Stores the client object that lets the GUI communicate with the server
@@ -44,12 +40,10 @@ public class LoginController {
     @FXML
     Button loginButton;
     @FXML
-    Button exitButton;
-    @FXML
     MenuItem menuClose;
-
     @FXML
     Label errorLabel;
+
 
 
 
@@ -68,13 +62,11 @@ public class LoginController {
     }
 
 
-
-
     @FXML
     //Closes the application
     private void exitButtonAction() throws IOException {
         //Doesn't try to close a connection that isn't there
-        if(client.isConnected()) {
+        if (client.isConnected()) {
             client.closeConnection(); // Closes Client connection safely
         }
         System.exit(0);
@@ -92,57 +84,46 @@ public class LoginController {
 
     private void loginButtonAction() throws IOException {
 
-        if(userTextField.getText().trim().isEmpty()) {
+        if (userTextField.getText().trim().isEmpty()) {
 
             errorLabel.setText("You have not entered a username!");
 
-        }
-
-        else if(userPassField.getText().trim().isEmpty()){
+        } else if (userPassField.getText().trim().isEmpty()) {
 
             errorLabel.setText("You have not entered a password!");
-        }
-
-        else if(!client.isConnected()){
+        } else if (!client.isConnected()) {
             errorLabel.setText("Cannot connect to server!");
-        }
-
-        else if(!client.isSameVersion()){
+        } else if (!client.isSameVersion()) {
             errorLabel.setText("Server and Client are different Versions!");
-        }
-
-
-        else {
+        } else {
 
             errorLabel.setText("");
 
 
-
-           currUser = new User(userTextField.getText(), userPassField.getText());
-
+            currUser = new User(userTextField.getText(), userPassField.getText());
 
 
-           String loginCode = client.requestLogin(currUser);
+            String loginCode = client.requestLogin(currUser);
 
 
-           if(loginCode.equals("BADLOGIN")){
-               errorLabel.setText("Unrecognised user details");
+            if (loginCode.equals("BADLOGIN")) {
+                errorLabel.setText("Unrecognised user details");
             }
 
-           //If not BADLOGIN assume GOODLOGIN
-           else {
+            //If not BADLOGIN assume GOODLOGIN
+            else {
                 errorLabel.setText("");
 
 
+                Stage currStage = (Stage) loginButton.getScene().getWindow();
+                currStage.close();
 
-               Stage currStage = (Stage) loginButton.getScene().getWindow();
-               currStage.close();
 
+                //Opens the main application once you have logged in
+                MainApplication app = new MainApplication();
+                Stage mainStage = new Stage();
+                app.transferInfoAndOpen(mainStage, client, currUser);
 
-               //Opens the main application once you have logged in
-               MainApplication app = new MainApplication();
-               Stage mainStage = new Stage();
-               app.transferInfoAndOpen(mainStage, client, currUser);
 
             }
         }
@@ -162,15 +143,32 @@ public class LoginController {
             stage.setTitle("Account Creation");
             stage.show();
 
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 
-    //Client field getter and setter, used by testing to create a new testing account manually.
     public void setClient(Client client) {
         this.client = client;
     }
-    public Client getClient() { return this.client; }
+
+
+    @FXML
+    private void venueLoginPageOpen() throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("venue-login-page.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load(), 300, 350);
+        VenueLoginController controller= fxmlLoader.getController();
+        controller.setClient(client);
+        stage.setScene(scene);
+        stage.setTitle("Venue Login Page");
+        stage.show();
+
+
+
+    }
+
+
 }
---
+++ src/main/java/GUI/VenueLoginController.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import client.Client;
+
+public class VenueLoginController {
+
+    Client client;
+
+
+    public void setClient(Client client){
+        this.client = client;
+    }
+
+
+
+
+
+
+}
