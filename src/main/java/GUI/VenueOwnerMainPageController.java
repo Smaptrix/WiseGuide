0fd14d4697c9a9ba729583cdeb248e182c6dacac
Jingham2510 <77728651+Jingham2510@users.noreply.github.com>
2022-03-23 15:35:59 +0000

    Venues can now login (if they exist) i.e. I manually added flares to the list

--- src/main/java/GUI/VenueOwnerMainPageController.java
+++ src/main/java/GUI/VenueOwnerMainPageController.java
@@ -1,6 +1,24 @@
 package GUI;
 
+import client.Client;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import serverclientstuff.User;
+
 public class VenueOwnerMainPageController {
 
+    Client client;
+    User currUser;
 
+    @FXML
+    Label titleLabel;
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public void setCurrUser(User currUser) {
+        this.currUser = currUser;
+        titleLabel.setText("VENUE ADMIN PAGE: " + currUser.getUsername());
+    }
 }
