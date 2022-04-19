fe6b36dd23c570b027773adb2920abde7aafd563
Jingham2510 <77728651+Jingham2510@users.noreply.github.com>
2022-04-19 20:22:40 +0100

    PreMerge Commits

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -516,28 +516,11 @@ public class Client {
 
 
     //Requests that the client can send a file to the server
+    //Maybe change so that it sends an email and then we discuss it
+    //Rather than having any user be able to upload any file they want
     public String requestUploadFile(File filetoUpload) throws IOException {
         outText.println("UPLOADFILE");
 
-        if (receiveAcknowledgement().equals("SIZE?")){
-            long fileSize = Files.size(Path.of(filetoUpload.getPath()));
-
-            byte[] fileSizeInBytes = ByteBuffer.allocate(4).putInt((int) fileSize).array();
-
-            int fileSizeInBytesLen = fileSizeInBytes.length;
-
-            //TODO - Rejig the clients I/O so that it can send bytes not just text
-
-            /*
-            //Tells the server how many bytes are determining the size of the file
-            outputStream.write(fileSizeInBytesLen);
-
-            //Writes the fileSize in bytes to the client
-            for (byte fileSizeInByte : fileSizeInBytes) {
-                outputStream.write(fileSizeInByte);
-            }
-            */
-        }
 
 
         return receiveAcknowledgement();
