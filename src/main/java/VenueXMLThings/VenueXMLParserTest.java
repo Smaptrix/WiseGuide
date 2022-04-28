29fb97e359d78fe11e4a48f390ae98c20dd4b081
bla511 <79090966+bla511@users.noreply.github.com>
2022-04-28 16:00:44 +0100

    Few more tests

--- src/main/java/VenueXMLThings/VenueXMLParserTest.java
+++ src/main/java/VenueXMLThings/VenueXMLParserTest.java
@@ -17,6 +17,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
 
 
 import static org.junit.Assert.*;
@@ -56,4 +59,28 @@ public class VenueXMLParserTest {
         assertEquals(this.XMLTestParser.removePage("wrongIndexType", "001"), -1);
     }
 
+    @Test
+    public void ChangeAttributeForNewVenue() throws TransformerException {
+        this.XMLTestParser.addPage("TestTitle", "TestID", "Testlat", "Testlon", "TestCategory", "TestPrice");
+        assertEquals(this.XMLTestParser.changeAttribute("title", "TestTitle", "lat", "newLat"), 1);
+        assertEquals(this.XMLTestParser.changeAttribute("title", "TestTitle", "NOCATEGORY", "newLat"), -1);
+        assertEquals(this.XMLTestParser.removePage("title", "TestTitle"), 1);
+    }
+
+    @Test
+    public void ChangeAttributeForOldVenue() throws TransformerException {
+        String oldLatitude = this.XMLTestParser.getPage("title", "Flares_York").attributes.get("lat");
+        assertEquals(this.XMLTestParser.changeAttribute("title", "Flares_York", "lat", "newLat"), 1);
+        assertEquals(this.XMLTestParser.changeAttribute("title", "Flares_York", "lat", oldLatitude), 1);
+    }
+
+    @Test
+    public void AddAndRemoveMediaToNewVenue() throws TransformerException {
+        this.XMLTestParser.addPage("TestTitle", "TestID", "Testlat", "Testlon", "TestCategory", "TestPrice");
+        Dictionary<String,String> attributes = new Hashtable<>();
+        attributes.put("include_source", "source/newsource.mp3");
+        attributes.put("include_source", "source/newsource.mp3");
+        this.XMLTestParser.addChildMedia("title", "TestTitle", );
+        assertEquals(this.XMLTestParser.removePage("title", "TestTitle"), 1);
+    }
 }
