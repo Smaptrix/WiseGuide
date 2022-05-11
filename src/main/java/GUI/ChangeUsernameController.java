e6e8ad76c1e349aeab60f2f1c2b5cabbd1614ed5
Jingham2510 <77728651+Jingham2510@users.noreply.github.com>
2022-05-11 16:22:17 +0100

    Added Major Comments to ChangeUsernameController

--- src/main/java/GUI/ChangeUsernameController.java
+++ src/main/java/GUI/ChangeUsernameController.java
@@ -1,3 +1,10 @@
+/*
+    Company Name:   Maptrix
+    Project Name:   WiseGuide
+    Authors:        Joe Ingham
+    Date Created:   18/02/2022
+    Last Updated:   11/05/2022
+ */
 package GUI;
 
 import client.Client;
@@ -10,42 +17,70 @@ import serverclientstuff.User;
 
 import java.io.IOException;
 
+/**
+ * Controls the page which the user uses to change their username
+ */
 public class ChangeUsernameController {
-
+    /**
+     * The current client
+     */
     Client client;
+    /**
+     * The current user
+     */
     User currUser;
 
-
+    /**
+     * The label which displays the current users username
+     */
     @FXML
     Label usernameLabel;
 
+    /**
+     * The text field which the user can type there desired name into
+     */
     @FXML
     TextField desiredNameField;
 
-
+    /**
+     * The label that displays any error information
+     */
     @FXML
     Label errLabel;
-
+    /**
+     * The button that ocnfirms the users request
+     */
     @FXML
     Button confirmButton;
 
-
+    /**
+     * This function runs first on the page startup
+     */
     @FXML
     public void initialize(){
         errLabel.setText("");
     }
 
-
+    /**
+     * Sets the current client for the controller
+     * @param client the client the controller is going to work with
+     */
     public void setClient(Client client) {
         this.client = client;
     }
 
+    /**
+     * Sets the current user for the controller
+     * @param currUser the current user of the controller
+     */
     public void setUser(User currUser){
         this.currUser = currUser;
         usernameLabel.setText(currUser.getUsername());
     }
 
-
+    /**
+     * Tells the client to request a name change when the button is pressed
+     */
     @FXML
     protected void onConfirmButtonPress(){
 
