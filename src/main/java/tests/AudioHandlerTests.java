e05c52b9d0b996be2140bcf1f35080eae36565d4
AidanCarson1 <aidancarson1@gmail.com>
2022-05-28 23:25:38 +0100

    Nothing notable but it's forcing me to commit before checking out another branch.

---
+++ src/main/java/tests/AudioHandlerTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import javafx.stage.Stage;
+import mediahandlers.AudioHandler;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import java.io.File;
+
+public class AudioHandlerTests extends ApplicationTest {
+
+    //TODO
+
+}
