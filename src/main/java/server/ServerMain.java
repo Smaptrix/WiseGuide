1bb0a9b38cda40c7e987285318820e401082f0ed
Jingham2510 <77728651+Jingham2510@users.noreply.github.com>
2022-06-03 17:35:58 +0100

    Updated Author Comment Added JavaDoc Comments

--- src/main/java/server/ServerMain.java
+++ src/main/java/server/ServerMain.java
@@ -3,7 +3,7 @@
     Project Name:   WiseGuide
     Authors:        Joe Ingham
     Date Created:   27/01/2022
-    Last Updated:   10/02/2022
+    Last Updated:   03/06/2022
  */
 
 package server;
@@ -13,29 +13,28 @@ import java.io.IOException;
 
 public class ServerMain {
 
-    //Entry point for server application
+    /**
+     * Entry point for the server half of the application
+     * @param args the arguments for the program if run in the command line
+     * @throws IOException if the server cannot connect to the client
+     */
     public static void main(String[] args) throws IOException {
-
-
             System.out.println("Server Start\n");
             new ServerMain();
-
     }
 
-    //TODO - ASk the client to login to the server
 
-
-    //Sets up the initial server
+    /**
+     * Creates the server object, and attaches it to a port
+     * The default port is 5555
+     */
     public ServerMain() {
-
-       //Restarts the server after a bit of time - REMEMBER TO RE-COMMENT BACK IN
-
+       //Restarts the server after a bit of time
             try {
                 while(true) {
                 Server server = new Server();
 
                 server.startup(5555);
-
 
                 server.bufferListen();
                 }
@@ -43,11 +42,7 @@ public class ServerMain {
                 System.out.println("Socket exception - Lost connection with client");
                 e.printStackTrace();
 
-    }
-
-
-
-
+            }
     }
 
 }
