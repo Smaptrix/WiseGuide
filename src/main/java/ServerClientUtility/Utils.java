996a593925c39dcc9fd57b0c15d8127bac37d241
Jingham2510 <77728651+Jingham2510@users.noreply.github.com>
2022-06-04 13:37:45 +0100

    Updated Author Comment Added JavaDoc Comments

--- src/main/java/ServerClientUtility/Utils.java
+++ src/main/java/ServerClientUtility/Utils.java
@@ -3,7 +3,7 @@
     Project Name:   WiseGuide
     Authors:        Joe Ingham
     Date Created:   08/02/2022
-    Last Updated:   10/02/2022
+    Last Updated:   04/06/2022
  */
 
 
@@ -14,12 +14,21 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-//A class containing some utility stuff
+/**
+ * <p>
+ *     This class provides generic utility tools that can be used by either the server or the client
+ *     Most of these can be accessed statically because a lot of them don't relate to eachother
+ *     This is a miscellaneous tool set
+ * </p>
+ */
 public class Utils {
 
 
-
-    //Opens a file in the system - MIGHT BE REDUNDANT SOON
+    /**
+     * Opens the given file on the system
+     * @param file the file to be opened
+     * @throws IOException if the file cannot be accessed
+     */
     public static void openFile(File file) throws IOException {
 
         Desktop.getDesktop().open(file);
@@ -27,7 +36,11 @@ public class Utils {
     }
 
 
-    //Converts a byte array into hex for password storage
+    /**
+     * <p>Converts a given byte array into hex format</p>
+     * @param byteArray the byte array to be transformed
+     * @return The hex string
+     */
     public static String bytesToHex(byte[] byteArray){
         StringBuilder hexString = new StringBuilder( 2 * byteArray.length);
         for (byte b : byteArray) {
